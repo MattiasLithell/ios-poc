660eedab1811f506e5ab41797724104302f1d289
Mattias Lithell <mattias.lithell@epiroc.com>
2020-08-24 15:57:19 +0200

    Updates

---
+++ precache-manifest.1a8639164461bf55a2080b2019903946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba3319de04f8dac8c6272b9b1b0c50d",
+    "url": "/ios-poc/index.html"
+  },
+  {
+    "revision": "5af3dc13691b45ad8e9a",
+    "url": "/ios-poc/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c230917362304dc5e13d",
+    "url": "/ios-poc/static/js/2.e00c8977.chunk.js"
+  },
+  {
+    "revision": "9545d2ff040f1d230f20c359158fcfad",
+    "url": "/ios-poc/static/js/2.e00c8977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5af3dc13691b45ad8e9a",
+    "url": "/ios-poc/static/js/main.938a9646.chunk.js"
+  },
+  {
+    "revision": "05400d899b516cd9ebba",
+    "url": "/ios-poc/static/js/runtime-main.952d4837.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ios-poc/static/media/logo.5d5d9eef.svg"
+  }
+]);
